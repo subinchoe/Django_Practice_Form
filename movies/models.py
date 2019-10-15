932380b82d4108a50db76379845844f21de322c6
subinchoe <ch960318@naver.com>
2019-10-15 17:54:35 +0900

    set model Movie

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    title_en = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    watch_grade = models.CharField(max_length=100)
+    score = models.FloatField()
+    audience = models.IntegerField()
+    open_date = models.DateField()
+    poster_url = models.TextField()
+    description = models.TextField()
