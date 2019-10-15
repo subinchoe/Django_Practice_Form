769fa56a2737c5e1066a9ede04185f9028397383
subinchoe <ch960318@naver.com>
2019-10-15 19:22:51 +0900

    create CommentForm

--- movies/forms.py
+++ movies/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Movie
+from .models import Movie, Comment
 
 class MovieForm(forms.ModelForm):
     open_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     class Meta:
         model = Movie
         fields = '__all__'
+
+class Comment(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
